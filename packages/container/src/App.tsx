09ea743bd6176f7eb89383a6f9cce6655db69c3c
oburu <oburusolo@gmail.com>
2022-10-10 21:50:10 +0100

    adding prefix for css prod

--- packages/container/src/App.tsx
+++ packages/container/src/App.tsx
@@ -2,14 +2,19 @@ import * as React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { MarketingMF } from './components/MarketingMF';
 import { Header } from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export function App() {
   return (
     <BrowserRouter>
-      <div>
+      <StylesProvider generateClassName={generateClassName}>
         <Header signedIn={false} onSignOut={console.log} />
         <MarketingMF />
-      </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 }
--- packages/marketing/src/App.tsx
+++ packages/marketing/src/App.tsx
@@ -1,21 +1,23 @@
 import * as React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma-mf',
+});
+
 export function App() {
   return (
-    <div>
-      <StylesProvider>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/pricing" component={Pricing} />
-            <Route path="/" component={Landing} />
-          </Switch>
-        </BrowserRouter>
-      </StylesProvider>
-    </div>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/pricing" component={Pricing} />
+          <Route path="/" component={Landing} />
+        </Switch>
+      </BrowserRouter>
+    </StylesProvider>
   );
 }
